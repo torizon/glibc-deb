5a55ada45e43a6591dc0e8def944fadf6738a64b
Aurelien Jarno <aurelien@aurel32.net>
2022-08-08 19:37:40 +0200

    debian/rules.d/debhelper.mk: make libc.so and ld.so executable also for libc6-udeb.  Closes: #1016868.

--- debian/rules.d/debhelper.mk
+++ debian/rules.d/debhelper.mk
@@ -95,6 +95,8 @@ $(patsubst %,$(stamp)binaryinst_%,$(DEB_UDEB_PACKAGES)): debhelper $(patsubst %,
 
 	dh_compress -p$(curpass)
 	dh_fixperms -p$(curpass)
+	find debian/$(curpass) -type f -name libc.so.* -exec chmod a+x '{}' ';'
+	find debian/$(curpass) -type f -regex '.*/ld.*\.so\.[0-9]' -exec chmod a+x '{}' ';'
 	dh_installdeb -p$(curpass)
 	# dh_shlibdeps -p$(curpass)
 	dh_gencontrol -p$(curpass)

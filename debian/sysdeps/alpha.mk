7af7b56d5f5fef7a4f3c011b36774e4556563d3d
Aurelien Jarno <aurelien@aurel32.net>
2023-05-14 22:54:46 +0200

    debian/sysdeps/alpha.mk: temporarily build for EV56 until the baseline is raised at the GCC level.

--- debian/sysdeps/alpha.mk
+++ debian/sysdeps/alpha.mk
@@ -1,2 +1,7 @@
 # configuration options for all flavours
 libc = libc6.1
+
+# temporarily build for EV56 until the baseline is raised at the GCC level
+# see https://lists.debian.org/debian-alpha/2023/05/msg00001.html
+CC = $(DEB_HOST_GNU_TYPE)-$(BASE_CC)$(DEB_GCC_VERSION) -mcpu=ev56 -mtune=ev56
+CXX = $(DEB_HOST_GNU_TYPE)-$(BASE_CXX)$(DEB_GCC_VERSION) -mcpu=ev56 -mtune=ev56

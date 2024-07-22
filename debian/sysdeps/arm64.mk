d2818b39668610c6e4484b88d128f879f7765d89
Aurelien Jarno <aurelien@aurel32.net>
2024-07-22 22:58:32 +0200

    debian/sysdeps/arm64.mk: compile with -mbranch-protection=standard on arm64 to enable PAC/BTI support.  Closes: #1063515.
    * debian/sysdeps/arm64.mk: compile with -mbranch-protection=standard on
      arm64 to enable PAC/BTI support.  Closes: #1063515.
    * debian/control.in/main: build-depends on g++13-for-host (>= 13.3.0-3) on
      arm64 to ensure PAC/BTI support in GCC.

--- debian/sysdeps/arm64.mk
+++ debian/sysdeps/arm64.mk
@@ -3,3 +3,6 @@ extra_config_options = --enable-multi-arch --enable-memory-tagging
 
 # main library
 libc_mvec = yes
+
+CC = $(DEB_HOST_GNU_TYPE)-$(BASE_CC)$(DEB_GCC_VERSION) -mbranch-protection=standard
+CXX = $(DEB_HOST_GNU_TYPE)-$(BASE_CXX)$(DEB_GCC_VERSION) -mbranch-protection=standard
